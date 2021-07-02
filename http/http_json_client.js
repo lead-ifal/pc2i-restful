725ecb99bc50064ff1c1d340f6e3a8177e5ddf3f
Felipe Alencar <felipealencarlopes@gmail.com>
2021-07-02 00:37:29 -0300

    - Importação de dados sintéticos do CSV.

--- http/http_json_client.js
+++ http/http_json_client.js
@@ -2,107 +2,80 @@
  * Simula com HTTP o envio de dados por um cliente PC2I (e.g., LoRa Node)
  * @author Felipe A. Lopes <felipe.alencar@ifal.edu.br>
  */
+const csv = require('csv-parser');
+const fs = require('fs');
 const http = require('http');
 
-// TODO Recuperar dados do csv criado pelo Jabes.
-const requestData = JSON.stringify({
-  "planting_area": {
-    "0": {
-    "size": 0,
-    "soil_characteristics": 0,
-    "location": "location",
-    "irrigation_zones": "zone" },
+function request(data) {
+  const requestData = JSON.stringify(data);
 
-    "1": {
-      "size": 0,
-      "soil_characteristics": 0,
-      "location": "location",
-      "irrigation_zones": "zone" }
-  },
-  
-  "cultivation": {
-    "0": {
-      "name": "name",
-      "type": "type",
-      "planting_date": "2021/04/08",
-      "harvest_date": "2021/04/08",
-      "culture_ratio": 0,
-      "cultivation_phase": "planting",
-      "soil_characteristics": 0,
-      "location": "location",
-      "change_date_phase": "2021/04/08" },
+  console.log(requestData);
 
-      "1": {
-        "name": "name",
-        "type": "type",
-        "planting_date": "2021/04/08",
-        "harvest_date": "2021/04/08",
-        "culture_ratio": 0,
-        "cultivation_phase": "planting",
-        "soil_characteristics": 0,
-        "location": "location",
-        "change_date_phase": "2021/04/08" }
-  },
+  const options = {
+    host: '127.0.0.1',
+    port: 8080,
+    path: '/update-entities',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  };
 
-  "irrigation_plan": {
-    "0": {
-      "datetime_last_irrigation": "2021/04/08 11:50:00",
-      "date_next_irrigation": "2021/04/08",
-      "irrigation_start_date": "2021/04/08",
-      "number_irrigations": 0,
-      "irrigation_duration": "30 minutes" },
+  const request = http.request(options, (res) => {
+    if (res.statusCode !== 201) {
+      console.log(res);
+      console.error(`Não recebi um Criado do servidor. Código: ${res.statusCode}`);
+      res.resume();
+      return;
+    }
 
-    "1": {
-      "datetime_last_irrigation": "2021/04/08 11:50:00",
-      "date_next_irrigation": "2021/04/08",
-      "irrigation_start_date": "2021/04/08",
-      "number_irrigations": 0,
-      "irrigation_duration": "30 minutes" }
-  },
-
-  "irrigation_zones": {
-    "0": {
-      "type_irrigation": "located",
-      "cultivation": "soybean",
-      "irrigation_plan": 0,
-      "height_sea_level": 0 },
-
-    "1": {
-      "type_irrigation": "located",
-      "cultivation": "soybean",
-      "irrigation_plan": 0,
-      "height_sea_level": 0 }
-  }
-});
-
-console.log(requestData);
-
-const options = {
-  host: '127.0.0.1',
-  port: 8080,
-  path: '/update-entities',
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json; charset=UTF-8'
-  }
-};
-
-const request = http.request(options, (res) => {
-  if (res.statusCode !== 201) {
-    console.log(res);
-    console.error(`Não recebi um Criado do servidor. Código: ${res.statusCode}`);
-    res.resume();
-    return;
-  }
-
-  res.on('close', () => {
-    console.log('Requisição realizada.');
+    res.on('close', () => {
+      console.log('Requisição realizada.');
+    });
   });
-});
 
-request.end(requestData);
+  request.end(requestData);
 
-request.on('error', (err) => {
-  console.error(`Erro encontrado ao tentar realizar uma requisição: ${err.message}`);
-});
+  request.on('error', (err) => {
+    console.error(`Erro encontrado ao tentar realizar uma requisição: ${err.message}`);
+  });
+}
+
+function getCSVData(file, type) {
+  let data = [];
+  return new Promise((resolve, reject) => {
+      fs.createReadStream(file)
+          .on('error', error => {
+              reject(error);
+          })
+          .pipe(csv({headers: false, separator: ',',}))
+          .on('data', (row) => {
+              let item = {
+                  hora: row[0],
+                  temperatura: row[1],
+                  umidade: row[2],
+                  uv: row[3],
+                  ph: row[4],
+                  umidade_solo: row[5]
+              };
+              data.push(item);
+              request(item);
+          })
+          .on('end', () => {
+              resolve(data);
+          });
+  });
+}
+
+async function getData() {
+  try { 
+      const data = await getCSVData("pc2i-restful/dados.csv", {});
+      console.log("testGetData: parsed CSV data:", data);
+      return data;
+  } catch (error) {
+      console.error("testGetData: An error occurred: ", error.message);
+  }
+}
+
+getData();
